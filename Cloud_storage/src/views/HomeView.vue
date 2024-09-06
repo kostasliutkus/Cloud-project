48a45e5134b1a29bcaaed965ad6f41b201ad4c9d
Kostas <kostliutk2@gmail.com>
2024-09-06 14:27:23 +0300

    init

---
+++ Cloud_storage/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+// import TheWelcome from '../components/TheWelcome.vue'
+import ListComponent from '../components/ListComponent.vue'
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    <ListComponent/>
+  </main>
+</template>
